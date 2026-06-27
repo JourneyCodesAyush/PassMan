3c3edff953f3f05a695a30a9ed9d7aeb59802665
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-06-27 14:59:06 +0530

    feat(db): add db initialization and table creation functions

---
+++ src/passman/schema.py
@@ -0,0 +1,25 @@
+from passman.database import execute
+
+
+def init_db() -> None:
+    _create_users_table()
+    _create_users_password_table()
+
+
+def _create_users_table() -> None:
+    query: str = """CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    salt TEXT NOT NULL
+    )"""
+    execute(query=query)
+
+
+def _create_users_password_table() -> None:
+    query: str = """CREATE TABLE IF NOT EXISTS passwords (
+    username TEXT NOT NULL,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    PRIMARY KEY (username, name)
+    )"""
+    execute(query=query)
